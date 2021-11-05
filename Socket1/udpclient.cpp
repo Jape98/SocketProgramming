9da113c418a213f71d520339ab5617e46b24f5d0
Jari Ojala <e1700833@edu.vamk.fi>
2021-11-05 10:35:02 +0200

    UDP client done

--- Socket1/udpclient.cpp
+++ Socket1/udpclient.cpp
@@ -1,8 +1,7 @@
+#pragma comment(lib, "ws2_32.lib")
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
 #include <stdio.h>
 #include <WinSock2.h>
-#pragma comment(lib, "sw2_32.lib")
-
-
 
 int main(int argc, char* argv[]) {
 	
@@ -22,4 +21,29 @@ int main(int argc, char* argv[]) {
 	printf("Winsock API ready \n");
 	SOCKET s = socket(AF_INET, SOCK_DGRAM, 0);
 
+	if (s == SOCKET_ERROR) {
+		printf("Socket cannot be created");
+		return 1;
+	}
+
+	// prepare remote(server) address, sa
+	SOCKADDR_IN sa;
+	sa.sin_family = AF_INET;
+	sa.sin_addr.s_addr = inet_addr(argv[1]);
+	sa.sin_port = htons(atoi(argv[2]));
+	printf("Udp client ready to go");
+
+	while(true) {
+
+		char sbuf[100];
+		printf("Message here >>");
+		fgets(sbuf, 100, stdin); //buffer,size and input
+		// socket to use, how many bytes to send out, flag = 0, remote address created earlier, size of my remote address.
+		ret = sendto(s, sbuf, strlen(sbuf),0,(SOCKADDR *)&sa,sizeof(sa));
+
+		if (ret == SOCKET_ERROR) {
+			printf("Sendto failed\n");
+			break;
+		}
+	}
 }
